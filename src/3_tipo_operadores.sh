c1e77ba69fca8328541fc2e36285976391b77093
gorgalla <carontecrypto@gmail.com>
2023-08-23 18:46:55 +0200

    add 3_tipo_operadores.sh + src

---
+++ src/3_tipo_operadores.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Programa para revisar los tipos de operadores
+#Autor: Gorka Gallardo - @gorgalla_
+numA=10
+numB=4
+echo "|********************************************|"
+echo -e "\n  Operadores aritmeticos"
+echo "  Numeros: A = $numA y B= $numB"
+echo -e "\n"
+echo "    A + B =" $((numA + numB))
+echo "    A - B =" $((numA - numB))
+echo "    A * B =" $((numA * numB))
+echo "    A / B =" $((numA / numB))
+echo "    A % B =" $((numA % numB))
+echo "|********************************************|"
+echo -e "\n  Operadores relacionales"
+echo "  Numero A = $numA || Numero B = $numB"
+echo -e "\n"
+echo "    A > B =" $((numA > numB))
+echo "    A < B =" $((numA < numB))
+echo "    A >= B =" $((numA >= numB))
+echo "    A <= B =" $((numA <= numB))
+echo "    A == B =" $((numA == numB))
+echo "    A != B =" $((numA != numB))
+echo "|********************************************|"
+echo -e "\n  Operadores de asignación"
+echo "  Numero A = $numA || Numero B = $numB"
+echo -e "\n"
+echo "    A += B =" $((numA += numB))
+echo "    A -= B =" $((numA -= numB))
+echo "    A *= B =" $((numA *= numB))
+echo "    A /= B =" $((numA /= numB))
+echo "    A %= B =" $((numA %= numB))
+echo "|********************************************|"
